146b290b5ae386cd874cbdaf5254f2493ccd7225
pveom <pveomegq@gmail.com>
2023-09-22 20:53:52 +0300

    L

---
+++ DZ_7/DZ_7-5/menu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_7_5
+{
+    enum ShopMenu
+    {
+        Start, Settings, About, Exit
+    }
+}
---
+++ LibleryMetod/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+void PrintMatrix(int[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < Array.GetUpperBound(1); j++)
+        {
+            Console.Write(Array[i, j] + "\t");
+        }
+        Console.WriteLine("\n");
+    }
+}
+
+int[,] RandomMatrix(int bound0, int bount1)
+{
+    Random rand = new Random();
+    int[,] masivRandom = new int[bound0, bount1];
+
+    for (int i = 0; i < masivRandom.GetUpperBound(0); i++)
+    {
+        for (int j = 0; j < masivRandom.GetUpperBound(1); j++)
+        {
+            masivRandom[i, j] = rand.Next(-10, 10);
+        }
+    }
+    return masivRandom;
+}
+
+void PrintArray(int[] Array)
+{
+    foreach (int i in Array)
+    {
+        Console.Write(i + "   ");
+    }
+}
+
+int[] RandomArrayTo100(int bound0)
+{
+    Random rand = new Random();
+
+    int[] masivRandom = new int[bound0];
+
+    for (int i = 0; i < masivRandom.Length; i++)
+    {
+        masivRandom[i] = rand.Next(100);
+    }
+    return masivRandom;
+}
+
