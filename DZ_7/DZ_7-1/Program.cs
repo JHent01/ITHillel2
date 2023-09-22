fb5f45d905668feab00267a4bb805864ae464c70
pveom <pveomegq@gmail.com>
2023-09-22 17:48:12 +0300

    DZ_7

---
+++ DZ_7/DZ_7-1/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+Random rand = new Random();
+
+int[] masivRandom = new int[20];
+
+for (int i = 0; i < masivRandom.Length; i++)
+{
+    masivRandom[i] = rand.Next(10);
+}
+int sum = 0;
+
+for (int i= 2;  i < masivRandom.Length;i+=2)
+{
+    sum += masivRandom[i];
+}
+
+PrintMass(masivRandom);
+Console.WriteLine("Сумма чисел на парних індексах = "+sum);
+void PrintMass(int[] longArray)
+{
+    foreach (int i in longArray)
+    {
+        Console.Write( i+", ");
+    }
+}
+
+
+Console.ReadKey();
