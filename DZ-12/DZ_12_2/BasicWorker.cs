6c86eec3bb034115e44d317d19e68fd6675d707d
pveom <pveomegq@gmail.com>
2023-10-16 14:20:52 +0300

    DZ-12

---
+++ DZ-12/DZ_12_2/BasicWorker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DZ_12_2
+{
+    internal class BasicWorker : DocumentWorker
+    {
+        
+        public override string OpenDocument(string open)
+        {
+            Console.WriteLine(base.openDocument);
+            return "";
+        }
+        public override string EditDocument(string edit)
+        {
+            Console.WriteLine(base.editDocument);
+            return "";
+        }
+
+
+
+        public override string SaveDocument(string save)
+        {
+            Console.WriteLine(base.saveDocument);
+            return "";
+        }
+    }
+}
---
+++ DZ-12/DZ_12_2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace DZ_12_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            Console.WriteLine("Введіть ключ доступу ");
+
+            string pass = Console.ReadLine();
+            if (pass == "Ex")
+            {
+                Console.WriteLine("Ви ввели ключ доступу Експерт");
+                ExpertDocumentWorker asd = new ExpertDocumentWorker();
+                
+            }
+            else if (pass == "Pro")
+            {
+                Console.WriteLine("Ви ввели ключ доступу Про");
+                ProDocumentWorker fof = new ProDocumentWorker();
+            }
+            else
+            {
+                Console.WriteLine("Ви ввели неправильний ключ доступу або не ввели ключ доступу, вам дали доступ Базовий  ");
+                BasicWorker ff = new BasicWorker();
+
+             
+            }
+        }
+
+
+    } 
+}
